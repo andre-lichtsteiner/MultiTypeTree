5d5957cd8811b2b1e024a1df01995a6a9882e07a
andre <andre-lichtsteiner@users.noreply.github.com>
2017-01-19 14:09:47 +1300

    Replaced SCMigrationModel, added LinearModelMatrix from backups made before

---
+++ src/beast/evolution/tree/LinearModelMatrix.java
@@ -0,0 +1,102 @@
+package beast.evolution.tree;
+
+import beast.core.BEASTObject;
+import beast.core.CalculationNode;
+import beast.core.Function;
+import beast.core.Input;
+import beast.core.parameter.BooleanParameter;
+import beast.core.parameter.RealParameter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by andre on 18/01/17.
+ */
+public class LinearModelMatrix extends CalculationNode implements Function {
+
+
+    //Must provide the model which is to be compared with the flat model
+    public Input<List<RealParameter>> rateMatricesInput = new Input<>(
+            "rateMatrix",
+            "Migration rate matrices",
+            new ArrayList<RealParameter>(), Input.Validate.REQUIRED);
+
+    public Input<List<RealParameter>> rateMatricesScaleFactorsInput = new Input<>("rateMatrixScaleFactor",
+            "Optional number by which all items in the migration matrix will be evenly multiplied.", new ArrayList<RealParameter>());
+    //List of rateMatrix scale factors
+
+
+    public Input<RealParameter> lambdaInput = new Input<>("lambdaParameter", "Specify a parameter with two starting values for lambda (the coefficient)", Input.Validate.REQUIRED);
+
+    public Input<BooleanParameter> deltaInput = new Input<>("deltaParameter", "Specify a parameter with two starting values for delta (the indicator variables)", Input.Validate.REQUIRED);
+
+    protected List<RealParameter> rateMatrices;
+    protected List<RealParameter> rateMatricesScaleFactors;
+    protected RealParameter lambdaParameter;
+    protected BooleanParameter deltaParameter;
+
+    public int getDimension(){
+        return rateMatricesInput.get().size(); //Returns number of matrices provided
+
+    }
+
+    public double getArrayValue(){
+        return rateMatricesInput.get().size(); //Need to determine what I should return here *TODO-----
+    }
+
+    @Override
+    public double getArrayValue(int dim) {
+        //Do some things here to change the
+        //Get the latest values for the delta and lambda parameters
+        lambdaParameter = lambdaInput.get();
+        deltaParameter = deltaInput.get();
+
+
+
+        double totalValue = 0;
+
+        for (int i = 0; i < rateMatrices.size(); i++){ //For each matrix provided
+            double lambda = lambdaParameter.getArrayValue(i);
+            double delta = deltaParameter.getArrayValue(i);
+            // double delta = 0;
+            //if (deltaParameter.getArrayValue(i)){
+            //    delta = 1;
+            // }
+
+            //Each matrix may have its own scaleFactor
+            double scaleFactor = 1; //Leave this as 1 if no scaleFactor provided
+            if (rateMatricesScaleFactorsInput.get() != null && rateMatricesScaleFactorsInput.get().size() != 0){
+                scaleFactor = rateMatricesScaleFactorsInput.get().get(i).getValue();
+            }
+
+
+            double term = lambda * delta * Math.log(rateMatrices.get(i).getArrayValue(dim)) * scaleFactor;
+            totalValue = totalValue + term;
+        }
+        return Math.exp(totalValue);
+        //return totalValue;
+    }
+
+    public void initAndValidate(){
+        //Setup initial state of this Function
+
+        rateMatrices = rateMatricesInput.get();
+        rateMatricesScaleFactors = rateMatricesScaleFactorsInput.get();
+
+
+        //Check that the XML file provides one (and only one) value for both the lambda and delta parameters for each of the matrices provided. If not, cannot proceed.
+        if (lambdaInput.get().getDimension() != rateMatricesInput.get().size() || deltaInput.get().getDimension() != rateMatricesInput.get().size()){
+            System.out.println("You must provide the LinearModelMatrix with one value for both the lambdaParameter and the deltaParameter for each of the rate matrices you provide.");
+            throw new IndexOutOfBoundsException("Wrong number of values provided to lambdaParameter or deltaParameter for LinearModelMatrix");
+        }
+
+        //Check that if there are any rateMatricesScaleFactors provided, that there are the right number of them
+        if(rateMatricesScaleFactorsInput.get() != null && rateMatricesScaleFactorsInput.get().size() != 0 && rateMatricesScaleFactorsInput.get().size() != rateMatricesInput.get().size()){
+            System.out.println("If you are using scaleFactors for a rateMatrix, you must provide the LinearModelMatrix with a rateMatrixScaleFactor for each of the rate matrices you provide.");
+            throw new IndexOutOfBoundsException("Wrong number of rateMatrixScaleFactor values provided for LinearModelMatrix");
+        }
+
+
+    }
+}
--- src/beast/evolution/tree/SCMigrationModel.java
+++ src/beast/evolution/tree/SCMigrationModel.java
@@ -18,7 +18,6 @@ package beast.evolution.tree;
 
 import beast.core.CalculationNode;
 import beast.core.Description;
-import beast.core.Function;
 import beast.core.Input;
 import beast.core.Input.Validate;
 import beast.core.parameter.BooleanParameter;
@@ -35,23 +34,35 @@ import org.jblas.MatrixFunctions;
 @Description("Basic plugin describing a simple Markovian migration model.")
 public class SCMigrationModel extends CalculationNode implements MigrationModel {
 
-    public Input<Function> rateMatrixInput = new Input<>(
+    public Input<RealParameter> rateMatrixInput = new Input<>(
             "rateMatrix",
-            "Migration rate matrix",
-            Validate.REQUIRED);
+            "Migration rate matrix");
 
-    public Input<Function> popSizesInput = new Input<>(
+    public Input<LinearModelMatrix> linearModelMatrixInput = new Input<>("linearModel", "Optional way to use GLM to compare the rateMatrix provided with one where all rates are the same.", Validate.XOR, rateMatrixInput); //Might be good to have Validate.XOR on this with rateMatrix
+
+    public Input<RealParameter> popSizesInput = new Input<>(
             "popSizes",
             "Deme population sizes.",
             Validate.REQUIRED);
-    
+
+    public Input<RealParameter> rateMatrixScaleFactorInput = new Input<>("rateMatrixScaleFactor",
+            "Optional number by which all items in the migration matrix will be evenly multiplied.");
+
+    public Input<RealParameter> popSizesScaleFactorInput = new Input<>("popSizesScaleFactor",
+            "Optional number by which all population sizes will be evenly multiplied.");
+
     public Input<BooleanParameter> rateMatrixFlagsInput = new Input<>(
             "rateMatrixFlags",
             "Optional boolean parameter specifying which rates to use."
-            + " (Default is to use all rates.)");
-    
-    protected Function rateMatrix, popSizes;
+                    + " (Default is to use all rates.)");
+
+
+
+
+    protected RealParameter rateMatrix, popSizes;
+    protected LinearModelMatrix linearModelMatrix;
     protected BooleanParameter rateMatrixFlags;
+    protected RealParameter rateMatrixScaleFactor, popSizesScaleFactor;
     protected double mu, muSym;
     protected int nTypes;
     protected DoubleMatrix Q, R;
@@ -59,9 +70,9 @@ public class SCMigrationModel extends CalculationNode implements MigrationModel 
     protected List<DoubleMatrix> RpowN, RsymPowN;
     protected DoubleMatrix RpowMax, RsymPowMax;
     protected boolean RpowSteady, RsymPowSteady;
-    
+
     protected boolean rateMatrixIsSquare, symmetricRateMatrix;
-    
+
     // Flag to indicate whether EV decompositions need updating.
     protected boolean dirty;
 
@@ -74,41 +85,55 @@ public class SCMigrationModel extends CalculationNode implements MigrationModel 
 
     @Override
     public void initAndValidate() {
-        
+
         popSizes = popSizesInput.get();
         rateMatrix = rateMatrixInput.get();
         nTypes = popSizes.getDimension();
 
+        popSizes.setLower(Math.max(popSizes.getLower(), 0.0));
+
+
+        //Split depending on type of matrix model
         if (rateMatrixFlagsInput.get() != null)
             rateMatrixFlags = rateMatrixFlagsInput.get();
 
-        if (rateMatrix instanceof RealParameter)
-            ((RealParameter)rateMatrix).setLower(Math.max(((RealParameter)rateMatrix).getLower(), 0.0));
 
-        if (popSizes instanceof RealParameter)
-            ((RealParameter)popSizes).setLower(Math.max(((RealParameter)popSizes).getLower(), 0.0));
-        
-        if (rateMatrix.getDimension() == nTypes*nTypes) {
-            rateMatrixIsSquare = true;
-            symmetricRateMatrix = false;
-        } else {
-            if (rateMatrix.getDimension() != nTypes*(nTypes-1)) {
-                if (rateMatrix.getDimension() == nTypes*(nTypes-1)/2) {
-                    symmetricRateMatrix = true;
-                } else 
-                    throw new IllegalArgumentException("Migration matrix has "
-                            + "incorrect number of elements for given deme count.");
-            } else {
-                rateMatrixIsSquare = false;
-                symmetricRateMatrix = false;
-            }
+        if (linearModelMatrixInput != null){
+            linearModelMatrix = linearModelMatrixInput.get();
+            //This should now be used instead of rateMatrix wherever rateMatrix is used
         }
-        
-        if (rateMatrixFlags != null) {
-            if (rateMatrixFlags.getDimension() != rateMatrix.getDimension())
-                throw new IllegalArgumentException("Migration rate flags"
-                        + " array does not have same number of elements as"
-                        + " migration rate matrix.");
+        else {
+
+            if (rateMatrixScaleFactorInput.get() != null)
+                rateMatrixScaleFactor = rateMatrixScaleFactorInput.get();
+
+            if (popSizesScaleFactorInput.get() != null)
+                popSizesScaleFactor = popSizesScaleFactorInput.get();
+
+            rateMatrix.setLower(Math.max(rateMatrix.getLower(), 0.0));
+
+            if (rateMatrix.getDimension() == nTypes * nTypes) {
+                rateMatrixIsSquare = true;
+                symmetricRateMatrix = false;
+            } else {
+                if (rateMatrix.getDimension() != nTypes * (nTypes - 1)) {
+                    if (rateMatrix.getDimension() == nTypes * (nTypes - 1) / 2) {
+                        symmetricRateMatrix = true;
+                    } else
+                        throw new IllegalArgumentException("Migration matrix has "
+                                + "incorrect number of elements for given deme count.");
+                } else {
+                    rateMatrixIsSquare = false;
+                    symmetricRateMatrix = false;
+                }
+            }
+
+            if (rateMatrixFlags != null) {
+                if (rateMatrixFlags.getDimension() != rateMatrix.getDimension())
+                    throw new IllegalArgumentException("Migration rate flags"
+                            + " array does not have same number of elements as"
+                            + " migration rate matrix.");
+            }
         }
 
         dirty = true;
@@ -138,7 +163,7 @@ public class SCMigrationModel extends CalculationNode implements MigrationModel 
                 if (i != j) {
                     Q.put(i, j, getBackwardRate(i, j));
                     Q.put(i, i, Q.get(i, i) - Q.get(i, j));
-                    
+
                     Qsym.put(i, j, 0.5*(getBackwardRate(i, j) + getBackwardRate(j, i)));
                     Qsym.put(i, i, Qsym.get(i, i) - Qsym.get(i, j));
                 }
@@ -146,7 +171,7 @@ public class SCMigrationModel extends CalculationNode implements MigrationModel 
 
             if (-Q.get(i, i) > mu)
                 mu = -Q.get(i, i);
-            
+
             if (-Qsym.get(i,i) > muSym)
                 muSym = -Qsym.get(i, i);
         }
@@ -154,18 +179,18 @@ public class SCMigrationModel extends CalculationNode implements MigrationModel 
         // Set up uniformized backward transition rate matrices R and Rsym:
         R = Q.mul(1.0/mu).add(DoubleMatrix.eye(nTypes));
         Rsym = Qsym.mul(1.0/muSym).add(DoubleMatrix.eye(nTypes));
-        
+
         // Clear cached powers of R and Rsym and steady state flag:
         RpowN.clear();
         RsymPowN.clear();
-        
+
         RpowSteady = false;
         RsymPowSteady = false;
-        
+
         // Power sequences initially contain R^0 = I
         RpowN.add(DoubleMatrix.eye(nTypes));
         RsymPowN.add(DoubleMatrix.eye(nTypes));
-        
+
         RpowMax = DoubleMatrix.eye(nTypes);
         RsymPowMax = DoubleMatrix.eye(nTypes);
 
@@ -179,7 +204,7 @@ public class SCMigrationModel extends CalculationNode implements MigrationModel 
     public int getNTypes() {
         return nTypes;
     }
-    
+
     /**
      * Obtain element of rate matrix for migration model.  Unlike getRate(),
      * this method does not return 0 when the BSSVS indicator variable is
@@ -190,12 +215,17 @@ public class SCMigrationModel extends CalculationNode implements MigrationModel 
      * @return Rate matrix element.
      */
     public double getRateForLog(int i, int j) {
-        
+
         if (i==j)
             return 0;
 
-        return rateMatrix.getArrayValue(getArrayOffset(i, j));
-        
+        if(linearModelMatrixInput.get() != null){
+            return linearModelMatrix.getArrayValue(getArrayOffset(i, j));
+        }
+        else {
+            return rateMatrix.getValue(getArrayOffset(i, j));
+        }
+
     }
 
     /**
@@ -210,15 +240,23 @@ public class SCMigrationModel extends CalculationNode implements MigrationModel 
     public double getBackwardRate(int i, int j) {
         if (i==j)
             return 0;
-        
+
         int offset = getArrayOffset(i, j);
         if (rateMatrixFlagsInput.get() != null
-                && !rateMatrixFlagsInput.get().getValue(offset))
+                && !rateMatrixFlagsInput.get().getValue(offset)) {
             return 0.0;
-        else
-            return rateMatrix.getArrayValue(offset);
+        }
+        else{
+            if (linearModelMatrixInput.get() != null){
+                return linearModelMatrix.getArrayValue(offset);
+            }
+            else{
+                return rateMatrix.getArrayValue(offset);
+            }
+        }
+
     }
-    
+
     /**
      * Obtain BSSVS flag corresponding to element of rate matrix for migration
      * model.  If flags are not being used, returns true.
@@ -228,12 +266,12 @@ public class SCMigrationModel extends CalculationNode implements MigrationModel 
      * @return Rate matrix element BSVS flag.
      */
     public boolean getRateFlag(int i, int j) {
-         
+
         if (i==j)
             return false;
-        
+
         if (rateMatrixFlagsInput.get() == null)
-           return true;
+            return true;
 
         return rateMatrixFlagsInput.get().getValue(getArrayOffset(i, j));
     }
@@ -252,29 +290,47 @@ public class SCMigrationModel extends CalculationNode implements MigrationModel 
 
         return getBackwardRate(j, i)*getPopSize(j)/getPopSize(i);
     }
-    
+
+    /**
+     * Set element of rate matrix for migration model.
+     * This method should only be called by operators.
+     *
+     * @param i
+     * @param j
+     * @param rate
+     */
+    public void setBackwardRate(int i, int j, double rate) {
+        if (i==j)
+            return;
+
+        rateMatrix.setValue(getArrayOffset(i,j), rate);
+
+        // Model is now dirty.
+        dirty = true;
+    }
+
     /**
      * Obtain offset into "rate matrix" and associated flag arrays.
-     * 
+     *
      * @param i
      * @param j
      * @return Offset (or -1 if i==j)
      */
     protected int getArrayOffset(int i, int j) {
-        
+
         if (i==j)
             throw new RuntimeException("Programmer error: requested migration "
                     + "rate array offset for diagonal element of "
                     + "migration rate matrix.");
-        
+
         if (rateMatrixIsSquare) {
             return i*nTypes+j;
         } else {
             if (symmetricRateMatrix) {
-             if (j<i)
-                 return i*(i-1)/2 + j;
-             else
-                 return j*(j-1)/2 + i;
+                if (j<i)
+                    return i*(i-1)/2 + j;
+                else
+                    return j*(j-1)/2 + i;
             } else {
                 if (j>i)
                     j -= 1;
@@ -282,7 +338,7 @@ public class SCMigrationModel extends CalculationNode implements MigrationModel 
             }
         }
     }
-    
+
     /**
      * Obtain effective population size of particular type/deme.
      *
@@ -290,7 +346,23 @@ public class SCMigrationModel extends CalculationNode implements MigrationModel 
      * @return Effective population size.
      */
     public double getPopSize(int i) {
-        return popSizes.getArrayValue(i);
+        if (popSizesScaleFactorInput.get() != null){
+            return popSizes.getArrayValue(i) * popSizesScaleFactorInput.get().getValue();
+        }
+        else{
+            return popSizes.getArrayValue(i);
+        }
+    }
+
+    /**
+     * Set effective population size of particular type/deme.
+     *
+     * @param i deme index
+     * @param newSize
+     */
+    public void setPopSize(int i, double newSize) {
+        popSizes.setValue(i, newSize);
+        dirty = true;
     }
 
     @Override
@@ -301,7 +373,7 @@ public class SCMigrationModel extends CalculationNode implements MigrationModel 
         else
             return mu;
     }
-    
+
     @Override
     public DoubleMatrix getR(boolean symmetric) {
         updateMatrices();
@@ -310,7 +382,7 @@ public class SCMigrationModel extends CalculationNode implements MigrationModel 
         else
             return R;
     }
-    
+
     @Override
     public DoubleMatrix getQ(boolean symmetric) {
         updateMatrices();
@@ -319,11 +391,11 @@ public class SCMigrationModel extends CalculationNode implements MigrationModel 
         else
             return Q;
     }
-    
+
     @Override
     public DoubleMatrix getRpowN(int n, boolean symmetric) {
         updateMatrices();
-        
+
         List <DoubleMatrix> matPowerList;
         DoubleMatrix mat, matPowerMax;
         if (symmetric) {
@@ -335,33 +407,33 @@ public class SCMigrationModel extends CalculationNode implements MigrationModel 
             mat = R;
             matPowerMax = RpowMax;
         }
-        
+
         if (n>=matPowerList.size()) {
-                
+
             // Steady state of matrix iteration already reached
             if ((symmetric && RsymPowSteady) || (!symmetric && RpowSteady)) {
                 //System.out.println("Assuming R SS.");
                 return matPowerList.get(matPowerList.size()-1);
             }
-                
+
             int startN = matPowerList.size();
             for (int i=startN; i<=n; i++) {
                 matPowerList.add(matPowerList.get(i-1).mmul(mat));
-                
+
                 matPowerMax.maxi(matPowerList.get(i));
-                    
+
                 // Occasionally check whether matrix iteration has reached steady state
                 if (i%10 == 0) {
                     double maxDiff = 0.0;
                     for (double el : matPowerList.get(i).sub(matPowerList.get(i-1)).toArray())
                         maxDiff = Math.max(maxDiff, Math.abs(el));
-                        
+
                     if (!(maxDiff>0)) {
                         if (symmetric)
                             RsymPowSteady = true;
                         else
                             RpowSteady = true;
-                        
+
                         return matPowerList.get(i);
                     }
                 }
@@ -372,7 +444,7 @@ public class SCMigrationModel extends CalculationNode implements MigrationModel 
 
     /**
      * Power above which R is known to be steady.
-     * 
+     *
      * @param symmetric
      * @return index of first known steady element.
      */
@@ -394,7 +466,7 @@ public class SCMigrationModel extends CalculationNode implements MigrationModel 
     /*
      * CalculationNode implementations.
      */
-    
+
     @Override
     protected boolean requiresRecalculation() {
         // we only get here if something is dirty
@@ -414,7 +486,7 @@ public class SCMigrationModel extends CalculationNode implements MigrationModel 
      * @param args
      */
     public static void main (String [] args) {
-        
+
         int n=10;
         DoubleMatrix Q = new DoubleMatrix(n, n);
         for (int i=0; i<n; i++) {
@@ -424,6 +496,6 @@ public class SCMigrationModel extends CalculationNode implements MigrationModel 
         }
         MatrixFunctions.expm(Q.mul(0.001)).print();
         Q.print();
-        
+
     }
 }
